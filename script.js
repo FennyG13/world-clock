705e95e71db9cc0df8e2d2cb65f651d6831e0271
OttoG13 <gao.fenny@gmail.com>
2024-02-14 20:46:56 -0500

    Add select city from dropdown function and update files

--- script.js
+++ script.js
@@ -1,31 +1,55 @@
 function updateTime() {
   // Boston
   let bostonElement = document.querySelector("#boston");
-  let bostonDateElement = bostonElement.querySelector(".date");
-  let bostonTimeElement = bostonElement.querySelector(".time");
-  let bostonTime = moment().tz("America/New_York");
+  if (bostonElement) {
+    let bostonDateElement = bostonElement.querySelector(".date");
+    let bostonTimeElement = bostonElement.querySelector(".time");
+    let bostonTime = moment().tz("America/New_York");
 
-  bostonDateElement.innerHTML = bostonTime.format("MMMM D, YYYY");
-  bostonTimeElement.innerHTML = bostonTime.format("h:mm:ss [<small>]A[</small>]");
+    bostonDateElement.innerHTML = bostonTime.format("MMMM D, YYYY");
+    bostonTimeElement.innerHTML = bostonTime.format("h:mm:ss [<small>]A[</small>]");
+  }
 
   // Fuzhou
   let fuzhouElement = document.querySelector("#fuzhou");
-  let fuzhouDateElement = fuzhouElement.querySelector(".date");
-  let fuzhouTimeElement = fuzhouElement.querySelector(".time");
-  let fuzhouTime = moment().tz("Asia/Taipei");
+  if (fuzhouElement) {
+    let fuzhouDateElement = fuzhouElement.querySelector(".date");
+    let fuzhouTimeElement = fuzhouElement.querySelector(".time");
+    let fuzhouTime = moment().tz("Asia/Taipei");
 
-  fuzhouDateElement.innerHTML = fuzhouTime.format("MMMM D, YYYY");
-  fuzhouTimeElement.innerHTML = fuzhouTime.format("h:mm:ss [<small>]A[</small>]");
+    fuzhouDateElement.innerHTML = fuzhouTime.format("MMMM D, YYYY");
+    fuzhouTimeElement.innerHTML = fuzhouTime.format("h:mm:ss [<small>]A[</small>]");
+  }
 
   // Lisbon
   let lisbonElement = document.querySelector("#lisbon");
-  let lisbonDateElement = lisbonElement.querySelector(".date");
-  let lisbonTimeElement = lisbonElement.querySelector(".time");
-  let lisbonTime = moment().tz("Europe/Lisbon");
+  if (lisbonElement) {
+    let lisbonDateElement = lisbonElement.querySelector(".date");
+    let lisbonTimeElement = lisbonElement.querySelector(".time");
+    let lisbonTime = moment().tz("Europe/Lisbon");
 
-  lisbonDateElement.innerHTML = lisbonTime.format("MMMM D, YYYY");
-  lisbonTimeElement.innerHTML = lisbonTime.format("h:mm:ss [<small>]A[</small>]");
+    lisbonDateElement.innerHTML = lisbonTime.format("MMMM D, YYYY");
+    lisbonTimeElement.innerHTML = lisbonTime.format("h:mm:ss [<small>]A[</small>]");
+  }
+}
+
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector(".default-cities");
+  citiesElement.innerHTML = `
+  <div class="cities">
+    <div>
+      <h2>${cityName}</h2>
+      <div class="date">${cityTime.format("MMMM D, YYYY")}</div>
+    </div>
+    <div class="time">${cityTime.format("h:mm:ss [<small>]A[</small>]")}</div>
+  </div>`;
 }
 
 updateTime();
 setInterval(updateTime, 1000);
+
+let selectedCityElement = document.querySelector(".dropdown-cities");
+selectedCityElement.addEventListener("change", updateCity);
